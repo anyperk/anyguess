8cf659d0fbcc78857d7b2a68d474047a1d793a5a
Shawn Catanzarite <me@shawncatz.com>
2018-07-11 15:17:47 -0700

    home#index. logo.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   resources :questions
   resources :users
   resources :games
@@ -9,5 +10,6 @@ Rails.application.routes.draw do
     end
   end
 
-  root 'play#index'
+  get 'home', to: 'home#index'
+  root 'home#index'
 end
