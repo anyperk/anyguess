c10563712e3ce06b6620c9342b128089e7165cd4
Shawn Catanzarite <me@shawncatz.com>
2018-07-11 12:44:04 -0700

    fix logout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def current_user
-    @current_user ||= User.find(session[:current_user_id])
+    @current_user ||= session[:current_user_id] && User.find(session[:current_user_id])
   end
 end
