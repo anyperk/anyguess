89ea239e6bec5828b119f36d81ae024b88187bcf
Shawn Catanzarite <me@shawncatz.com>
2018-07-11 13:12:17 -0700

    avatars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,18 @@ module ApplicationHelper
   def current_user
     @current_user ||= session[:current_user_id] && User.find(session[:current_user_id])
   end
+
+  def gravatar_url(email, size = 32)
+    gravatar = Digest::MD5::hexdigest(email).downcase
+    "http://gravatar.com/avatar/#{gravatar}.png?s=#{size}&d=robohash"
+  end
+
+  def avatar(email, size = 32)
+    g = gravatar_url(email, size)
+    # if g == "blarg"
+    #   render partial: 'users/avatar'
+    # else
+    image_tag g, alt: current_user.email
+    # end
+  end
 end
