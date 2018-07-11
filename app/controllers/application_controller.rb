460ff2b9419cf95152b7e1bd0ba16f9dbafa2ae1
Shawn Catanzarite <me@shawncatz.com>
2018-07-11 12:44:18 -0700

    fix logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,6 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= User.find(session[:current_user_id])
+    @current_user ||= session[:current_user_id] && User.find(session[:current_user_id])
   end
 end
