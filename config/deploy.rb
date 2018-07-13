3c21029f1c8c5e6f5305f8df4df64eb845e14797
Shawn Catanzarite <me@shawncatz.com>
2018-07-12 23:04:39 -0700

    fuck...

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :pty, true
 # set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
