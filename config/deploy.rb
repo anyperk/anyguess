6e5607bb1ae5c174efbd0f4fb60c7d50414b8934
Shawn Catanzarite <me@shawncatz.com>
2018-07-12 21:14:59 -0700

    more cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :rails_env, 'production'
 set :deploy_to, "/srv/apps/#{fetch(:application)}"
 
 # Default value for :scm is :git
-set :scm, :git
+# set :scm, :git
 
 # Default value for :format is :pretty
 # set :format, :pretty
